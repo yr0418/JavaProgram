3a16258bf771b33f6311af6c8001fc71c6fd9a72
yr0418 <1933352594@qq.com>
2019-09-16 12:40:11 +0800

    添加登陆接口

--- src/main/java/com/hubu/work/mybatis/pojo/User.java
+++ src/main/java/com/hubu/work/mybatis/pojo/User.java
@@ -6,7 +6,6 @@ import javax.persistence.Id;
 
 public class User {
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
--- src/main/java/com/hubu/work/web/controller/UserController.java
+++ src/main/java/com/hubu/work/web/controller/UserController.java
@@ -3,10 +3,9 @@ package com.hubu.work.web.controller;
 import com.hubu.work.mybatis.pojo.User;
 import com.hubu.work.web.common.BaseController;
 import com.hubu.work.web.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,11 @@ public class UserController extends BaseController<User> {
   @GetMapping(value = "selectAll")
   public List<User> selectAll(){
     return userService.selectAll();
+  }
+
+  @ApiOperation(value = "登陆")
+  @PostMapping(value = "login")
+  public int login(@RequestBody User user){
+    return userService.findUser(user);
   }
 }
