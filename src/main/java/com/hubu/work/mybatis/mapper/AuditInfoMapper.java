2c5e7fc4dba1a03afe2fa5514108345492209c59
Liu599769081 <599769081@qq.com>
2019-11-21 22:36:39 +0800

    完善 AuditInfo 模块的功能

--- src/main/java/com/hubu/work/mybatis/mapper/AuditInfoMapper.java
+++ src/main/java/com/hubu/work/mybatis/mapper/AuditInfoMapper.java
@@ -10,9 +10,10 @@ import java.util.List;
 /**
  * @moduleName AuditInfoMapper
  * @description audit_info 对应的 mapper层
- * @Author 杨睿
+ * @Author 刘旭山
  */
 public interface AuditInfoMapper extends CustomMapper<AuditInfo> {
+
   /**
    * 查询用户接受到的申请消息的总数
    *
@@ -20,60 +21,4 @@ public interface AuditInfoMapper extends CustomMapper<AuditInfo> {
    * @return int
    */
   int getAuditInfoCountWithReceive(@Param("username") String username);
-
-  /**
-   * 查询用户接收到的申请信息中未读消息的数量
-   *
-   * @param username 用户名
-   * @return int
-   */
-  int getAuditInfoCountWithReceiveAndUnread(@Param("username") String username);
-
-  /**
-   * 查询用户发送过的申请消息的总数
-   *
-   * @param username 用户名
-   * @return int
-   */
-  int getAuditInfoCountWithSend(@Param("username") String username);
-
-  /**
-   * 查询用户发送出去的未审核的申请消息的总数
-   *
-   * @param username 用户名
-   * @return int
-   */
-  int getAuditInfoCountWithSendAndUnread(@Param("username") String username);
-
-  /**
-   * 获取用户接受到的未读的入队申请信息
-   *
-   * @param username 用户名
-   * @return List
-   */
-  List<AuditInfo> selectAuditInfoWithReceiveAndUnread(@Param("username") String username);
-
-  /**
-   * 获取用户接收到的已审核的入队申请信息
-   *
-   * @param username username
-   * @return List
-   */
-  List<AuditInfo> selectAuditInfoWithReceiveAndRead(@Param("username") String username);
-
-  /**
-   * 获取用户发送的未读审核的入队申请信息
-   *
-   * @param username 用户名
-   * @return List
-   */
-  List<AuditInfo> selectAuditInfoWithSendAndUnread(@Param("username") String username);
-
-  /**
-   * 获取用户发送的已被审核的入队申请信息
-   *
-   * @param username
-   * @return
-   */
-  List<AuditInfo> selectAuditInfoWithSendAndRead(@Param("username") String username);
 }
--- src/main/java/com/hubu/work/web/service/AuditInfoService.java
+++ src/main/java/com/hubu/work/web/service/AuditInfoService.java
@@ -12,7 +12,7 @@ import java.util.List;
  * @moduleName AuditInfoService
  * @description audit_info 对应的Service层
  *
- * @Author 杨睿
+ * @Author 刘旭山
  * @since 2019-10-09 17:04
  **/
 @Service
@@ -20,37 +20,8 @@ public class AuditInfoService extends BaseService<AuditInfo> {
   @Autowired
   AuditInfoMapper auditInfoMapper;
 
-
   public int getAuditInfoCountWithReceive(String username){
     return auditInfoMapper.getAuditInfoCountWithReceive(username);
-  }
-
-  public int getAuditInfoCountWithReceiveAndUnread(String username){
-    return auditInfoMapper.getAuditInfoCountWithReceiveAndUnread(username);
-  }
-
-  public int getAuditInfoCountWithSend(String username){
-    return auditInfoMapper.getAuditInfoCountWithSend(username);
-  }
-
-  public int getAuditInfoCountWithSendAndUnread(String username){
-    return auditInfoMapper.getAuditInfoCountWithSendAndUnread(username);
-  }
-
-  public List<AuditInfo> selectAuditInfoWithReceiveAndUnread(String username){
-    return auditInfoMapper.selectAuditInfoWithReceiveAndUnread(username);
-  }
-
-  public List<AuditInfo> selectAuditInfoWithReceiveAndRead(String username){
-    return auditInfoMapper.selectAuditInfoWithReceiveAndRead(username);
-  }
-
-  public List<AuditInfo> selectAuditInfoWithSendAndUnread(String username){
-    return auditInfoMapper.selectAuditInfoWithSendAndUnread(username);
-  }
-
-  public List<AuditInfo> selectAuditInfoWithSendAndRead(String username){
-    return auditInfoMapper.selectAuditInfoWithSendAndRead(username);
   }
 
 
